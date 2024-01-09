304619aa32ba345dd82f4a6b087e2f407b19cf21
Raphael Frajuca <raphael.frajuca@solucx.com.br>
2024-01-09 17:45:52 -0300

    Correção nome do DTO

--- src/domain/scheduler/controllers/schedule.controller.ts
+++ src/domain/scheduler/controllers/schedule.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
-import { CreateSchedulerDto } from "src/domain/scheduler/dtos/scheduler.dto";
+import { SchedulerDto } from "src/domain/scheduler/dtos/scheduler.dto";
 import { ScheduleService } from "src/domain/scheduler/services/schedule.service";
 
 @ApiTags("schedule")
@@ -9,7 +9,7 @@ export class ScheduleController {
     constructor(private readonly scheduleService: ScheduleService) {}
 
     @Post()
-    createScheduler(@Body() createSchedulerDto: CreateSchedulerDto) {
-        return this.scheduleService.createScheduler(createSchedulerDto);
+    createScheduler(@Body() schedulerDto: SchedulerDto) {
+        return this.scheduleService.createScheduler(schedulerDto);
     }
 }
