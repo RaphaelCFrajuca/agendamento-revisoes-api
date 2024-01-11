f28b60b581ece109c78532bee6a588009fcfa071
Raphael Frajuca <raphael.frajuca@solucx.com.br>
2024-01-11 20:09:15 -0300

    Preparação para adicionar testes

---
+++ test/sheduler/unit/schedule-service.unit.spec.ts
@@ -0,0 +1,6 @@
+describe("Hello World Test", () => {
+    it('should return "Hello, World!"', () => {
+        const result = "Hello, World!";
+        expect(result).toBe("Hello, World!");
+    });
+});
